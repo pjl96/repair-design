53f1b2ff0bfcb744c454070a7c2508dd9e21f707
pjl96 <zakharov.oleg1996@gmail.com>
2020-03-02 18:28:22 +0300

    Added task for min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
 // Static server
 gulp.task('browser-sync', function() {
@@ -9,4 +11,11 @@ gulp.task('browser-sync', function() {
         }
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
+});
+ 
+gulp.task('default', function () { 
+    gulp.src('./css/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
 });
