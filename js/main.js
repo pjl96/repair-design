4e17f7bfe46ddd75ac9d28bf5830b8a27314a27d
pjl96 <zakharov.oleg1996@gmail.com>
2020-03-12 15:52:28 +0300

    feat: add functions close modal on ESC & click, add jquery

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  } 
+$(document).ready(function () {
+  var modal = $('.modal');
+  var modalBtn = $('[data-toggle=modal]');
+  var closeBtn = $('.modal__close');
 
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
+  // Обработка клика по кнопке и вызов модального окна
+  modalBtn.on('click', function() {
+    modal.toggleClass('modal--visible');
   });
 
-  closeBtn.addEventListener('click', switchModal);
+  // Обработка клика по крестику и скрытие модального окна
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+
+  // Обработка нажатия клавиши Esc
+  // Если открыто модальное окно - закрывает его
+  $(document).keydown(function (e) { 
+    if (e.keyCode === 27) {
+      modal.removeClass('modal--visible');
+    };
+  });
   
-})
+  // Обработка клика, при открытом модальном окне
+  $(modal).mouseup(function (e) { 
+    var modalDialog = $('.modal__dialog')
+    //Клик был не по модальному окну, и его дочерним элементам
+    if (!modalDialog.is(e.target) && modalDialog.has(e.target).length === 0) {
+      modal.toggleClass('modal--visible'); // Скрываем модальное окно
+    }
+  });
+
+});
