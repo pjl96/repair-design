66919da32205801aee75b327a6a55e99fe9c6229
pjl96 <zakharov.oleg1996@gmail.com>
2020-03-14 06:18:18 +0300

    feat: add section projects

--- js/main.js
+++ js/main.js
@@ -46,4 +46,24 @@ $(document).ready(function () {
     });
   });
 
+  //Подключение слайдера
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+  
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 30 + bullets.width() + 30)
+  bullets.css('left', prev.width() + 30)
+
 });
