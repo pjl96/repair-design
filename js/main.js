ac3851651405414f8c3a1a4e1843ccc1b177f1d8
pjl96 <zakharov.oleg1996@gmail.com>
2020-03-23 13:23:03 +0300

    feat: constructor map

--- js/main.js
+++ js/main.js
@@ -152,40 +152,6 @@ $(document).ready(function () {
   // Яндекс карта
   // Функция ymaps.ready() будет вызвана, когда
   // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-  ymaps.ready(init);
-  function init(){
-    // Создание карты.
-    var myMap = new ymaps.Map('map', {
-      center: [47.244729, 39.723187],
-      zoom: 16
-    }, {
-      searchControlProvider: 'yandex#search'
-    }),
-
-    // Создаём макет содержимого.
-    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-    ),
-
-    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-      hintContent: 'Наш офис',
-      balloonContent: 'Вход со двора'
-    }, {
-      // Опции.
-      // Необходимо указать данный тип макета.
-      iconLayout: 'default#image',
-      // Своё изображение иконки метки.
-      iconImageHref: '../img/marker.png',
-      // Размеры метки.
-      iconImageSize: [32, 32],
-      // Смещение левого верхнего угла иконки относительно
-      // её "ножки" (точки привязки).
-      iconImageOffset: [-5, -38]
-    });
-
-  myMap.geoObjects
-  .add(myPlacemark)
-  .add(myPlacemarkWithContent);
-  }
+  
 
 });
