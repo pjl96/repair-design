6f550886ed903bf070bf8f89f53a9a902a2d5876
pjl96 <zakharov.oleg1996@gmail.com>
2020-03-27 05:15:15 +0300

    feat: add minify svg and load map after 2 seconds

--- js/main.js
+++ js/main.js
@@ -226,6 +226,13 @@ $(document).ready(function () {
   // Маска для телефона
   $('[type=tel]').mask('+7 (000) 000-00-00');
 
+
+  setTimeout(function(){
+    var elem = document.createElement('script');
+    elem.type = 'text/javascript';
+    elem.src = 'https://api-maps.yandex.ru/2.1/?apikey=1d75a2ac-e4dd-445e-bd66-4043c00de24b&lang=ru_RU';
+    document.getElementsByTagName('body')[0].appendChild(elem);
+  }, 2000);
   // Яндекс карта
   // Функция ymaps.ready() будет вызвана, когда
   // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
